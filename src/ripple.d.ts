903c020ecac7b81eefbc16ecc8ca99d4f9043a79
Florian Reifschneider <florian@rocketloop.de>
2019-08-30 15:57:28 +0200

    hotfix: Fixes issue with typings

--- src/ripple.d.ts
+++ src/ripple.d.ts
@@ -1,11 +1,12 @@
 import { ContentView } from 'tns-core-modules/ui/content-view';
 import { Property } from 'tns-core-modules/ui/core/properties';
+import { Color } from 'tns-core-modules/color';
 
 /**
  * Represents the Ripple layout.
  */
 export class Ripple extends ContentView {
-  public static rippleColorProperty: Property;
+  public static rippleColorProperty: Property<Ripple, Color>;
 
   /**
    * Gets the native [android widget](com.balysv.materialripple.MaterialRippleLayout) that represents the user interface for this component. Valid only when running on Android OS.
